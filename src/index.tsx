1fdafd54c362097fddedb6c039e62d75f2c043cd
Sergei Galkovskii <sergei.galkovskii@mocoding.com>
2017-04-19 22:17:27 +0300

    Initialize the project

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends React.Component<{}, {}> {
+    public render() {
+        return (
+            <View>
+                <Text>
+                    hello, world!
+                </Text>
+            </View>
+        );
+    }
+}
